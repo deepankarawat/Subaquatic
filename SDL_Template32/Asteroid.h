0a069a8777bfadda129fe28b735d67f4cc9f7157
deepankarawat <56966784+deepankarawat@users.noreply.github.com>
2020-03-18 19:11:12 -0400

    Add files via upload

---
+++ SDL_Template32/Asteroid.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SDL_image.h>
+
+class Asteroid
+{
+	SDL_Texture* pAsteroidTex = nullptr;
+	SDL_Rect src;
+	SDL_Rect dst;
+
+public:
+
+	SDL_Rect getRect();
+	Asteroid();
+	Asteroid(SDL_Renderer* renderer);
+
+	// destructor -- used when the object is to be removed from memory
+	~Asteroid();
+
+	void draw(SDL_Renderer* renderer);
+
+	void cleanup();
+
+};
+
